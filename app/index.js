7e3410469a2de7032f60935adadc6a4f6c9f37e2
namo5758 <76212150+namo5758@users.noreply.github.com>
2023-04-04 19:12:16 +0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+app.use('/static', express.static('static'));
+hbs.registerPartials('views/partials')
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.listen(port, () => {
+    console.log(`http://127.0.0.1:${port}`)
+})
