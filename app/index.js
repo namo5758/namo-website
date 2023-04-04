218d7766a974708ffc06e2528a089f993895de7f
namo5758 <76212150+namo5758@users.noreply.github.com>
2023-04-04 19:41:02 +0700

    Add dotenv

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const hbs = require("hbs");
+const dotenv = require("dotenv").config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT ?? 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
